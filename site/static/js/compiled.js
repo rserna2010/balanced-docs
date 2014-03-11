5e62510e8b2f08b7123035a5124ca704ee1e13de
Richie <rserna2010@gmail.com>
2014-03-11 10:01:25 -0700

    Edit styling

--- site/static/js/compiled.js
+++ site/static/js/compiled.js
@@ -24,8 +24,13 @@ var lunr=function(t){var e=new lunr.Index;return e.pipeline.add(lunr.stopWordFil
 
 $('.search link').attr('href','/static/css/styles.css')
 $('.gsc-input').attr('background-color', '#59606B')
-$('#gsc-i-id1').attr('style', "width: 100%; padding: 0px; border: none; margin: -0.0625em 0px 0px; height: 1.25em; outline: none; background-color: rgb(255, 255, 255); background-position: 0% 50%; background-repeat: no-repeat no-repeat;")
-//////////////
+
+$( document ).ready(function() {
+    setTimeout(function(){
+        $('.gsc-input').attr('background-color', '#59606B')
+        $('#gsc-i-id1').attr('style', "width: 100%; padding: 0px; border: none; margin: -0.0625em 0px 0px; height: 1.25em; outline: none; background-color: #59606B; background-position: 0% 50%; background-repeat: no-repeat no-repeat;")
+    }, 100)
+});//////////////
 // docs.js //
 /////////////
 
